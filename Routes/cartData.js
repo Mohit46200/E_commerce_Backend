eb74be4c81ff649fc22ac341eea759836460ddc0
Mohit <mohit22600@gmail.com>
2026-06-02 08:48:06 +0530

    bugs fixing

--- Routes/cartData.js
+++ Routes/cartData.js
@@ -11,7 +11,7 @@ router.post("/cartdata", async (req, res) => {
                 cartdata.product_id = []
                 await cartdata.save()  
             }else if(remove){
-                    cartdata.product_id = cartdata.product_id.filter(id => id !== product_id)
+                    cartdata.product_id = cartdata.product_id.filter(item => item[0] !== product_id[0])
                     await cartdata.save()  
             }else{
                 cartdata.product_id.push(product_id)
